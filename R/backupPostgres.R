d86740457bc4608665d8bd14a8fd60ea889fedda
André Calero Valdez <andrecalerovaldez@gmail.com>
2024-05-02 17:28:58 +0200

    added a backup script

--- R/backupPostgres.R
+++ R/backupPostgres.R
@@ -7,41 +7,37 @@ library(tidyverse)
 library(DBI)
 library(RSQLite)
 library(RPostgres)
+library(here)
 
 host_name <- "aws-0-eu-central-1.pooler.supabase.com"
 db_user <- Sys.getenv("SUPABASEUSER")
 db_password <- Sys.getenv("SUPABASEPW")
 
-target <- dbConnect(RPostgres::Postgres(), 
+db <- dbConnect(RPostgres::Postgres(), 
                 dbname = "postgres", host = host_name, port = 5432, 
                 user = db_user, 
                 password = db_password,
                 gssencmode="disable"
 )
 
-db <- dbConnect(RSQLite::SQLite(), here("data", "my_data.db"))
+target <- dbConnect(RSQLite::SQLite(), here("data", "backups", glue::glue("backup-{today()}.db")))
 #list all tables
 tables <- dbListTables(db) %>% 
   tibble(tbl_name = .) %>% 
-  filter(!str_detect(tbl_name, "sqlite")) 
+  filter(!str_detect(tbl_name, "pg_stat")) 
 
-dbBegin(target)
+#dbBegin(target)
 for(tab in tables$tbl_name) {
   cat("Copying ", tab, "\n")
   tbl <- tbl(db, tab) |> collect()
   cat("Rows: ", nrow(tbl), "\n")
   #copy_to(target, tbl, tab, overwrite = TRUE)
   
-  if(tab == "teaching_evaluations"){
-    tbl <- tbl %>% mutate(id = row_number()) %>% 
-      select(id, everything())
-  }
   dbWriteTable(target, tab, tbl, overwrite = TRUE)  
-  dbExecute(target, glue::glue("ALTER TABLE {tab} ADD PRIMARY KEY (id);"))
+  #dbExecute(target, glue::glue("ALTER TABLE {tab} ADD PRIMARY KEY (id);"))
   
 }
 
-dbCommit(target)
 dbDisconnect(db)
 dbListTables(target)
 dbDisconnect(target)
